8d8242fbe8a9e1c7e58b01c6651d8bf95cd25412
Uggla <uggla@free.fr>
2025-04-28 16:27:44 +0200

    fix: Change presentation name for pdf generation

--- build_pdf/build_pdf.sh
+++ build_pdf/build_pdf.sh
@@ -21,7 +21,7 @@ current_dir=$(pwd)
 cd "$(dirname "$0")"
 cd ../slides
 
-podman run -ti --userns=keep-id --rm -v "$PWD":/data:Z -e "HOSTNAME=$HOSTNAME" -e "OUTPUT=bevy_university" localhost/build_pdf:latest
+podman run -ti --userns=keep-id --rm -v "$PWD":/data:Z -e "HOSTNAME=$HOSTNAME" -e "OUTPUT=python_dojo_01" localhost/build_pdf:latest
 
 cd "$current_dir"
 exit 0
